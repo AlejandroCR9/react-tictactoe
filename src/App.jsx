5767d7601272c5d84f8b8acc9a0f02ca87e77fcf
Alejandro Cedillo <aleejandro.99r@gmail.com>
2023-05-25 13:45:21 -0600

    Add persistent local storage

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,36 @@ import { WinnerModal } from './components/WinnerModal.jsx'
 
 import { TURNS } from './constants.js'
 import { checkWinnerFrom, checkEndGame } from './logic/board.js'
+import { resetGameStorage, saveGameStorage } from './logic/storage/index.js'
 
 import './App.css'
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage 
+           ? JSON.parse(boardFromStorage) 
+           : Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
   const [winner, setWinner] = useState(null)
 
   const updateBoard = (index) => {
 
     if(board[index] || winner) return
-    
+
     const newBoard = [... board]
     newBoard[index] = turn
     setBoard(newBoard)
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn)
+    saveGameStorage({
+        board: newBoard,
+        turn: newTurn
+    })
     const newWinner = checkWinnerFrom(newBoard)
     if(newWinner){
       confetti()
@@ -37,6 +50,7 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+    resetGameStorage()
   }
 
   return (
